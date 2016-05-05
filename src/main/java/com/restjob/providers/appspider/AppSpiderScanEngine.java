e88fdfc28f5a932554b16fe9f9a4823d2e23711f
Steve Springett <steve@Prometheus.local>
2016-05-05 16:31:55 -0500

    Added appspider specific scan engine

---
+++ src/main/java/com/restjob/providers/appspider/AppSpiderScanEngine.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of RESTjob Controller.
+ *
+ * RESTjob Controller is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * RESTjob Controller is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * RESTjob Controller. If not, see http://www.gnu.org/licenses/.
+ */
+package com.restjob.providers.appspider;
+
+import com.restjob.providers.RemoteScanEngine;
+
+import javax.xml.namespace.QName;
+
+public class AppSpiderScanEngine extends RemoteScanEngine {
+
+    private QName serviceName = new QName("http://ntobjectives.com/webservices/", "NTOService");
+
+    public QName getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(QName serviceName) {
+        this.serviceName = serviceName;
+    }
+
+}
