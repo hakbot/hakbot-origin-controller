cc76259ce41c4b2b2c56808c4d11d7c2ac1ca676
stevespringett <steve@springett.us>
2016-06-20 17:10:01 -0500

    Added utils to strip out and replace dashes in uuids

---
+++ src/main/java/io/hakbot/util/UuidUtil.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Hakbot Origin Controller.
+ *
+ * Hakbot Origin Controller is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Hakbot Origin Controller is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Hakbot Origin Controller. If not, see http://www.gnu.org/licenses/.
+ */
+package io.hakbot.util;
+
+public class UuidUtil {
+
+    private UuidUtil() { }
+
+    public static String insertHyphens(String uuidWithoutHyphens) {
+        return uuidWithoutHyphens.replaceFirst( "(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5" );
+    }
+
+    public static String stripHyphens(String uuid) {
+        return uuid.replaceAll("-", "");
+    }
+
+}
