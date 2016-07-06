6b96675d218ca5fc8189c690e19a23eddd30caa8
stevespringett <steve@springett.us>
2016-07-05 23:46:50 -0500

    Added working AppSpider Pro console

---
+++ src/main/webapp/WEB-INF/plugins/io.hakbot.providers.appspider.AppSpiderProvider/functions.js
@@ -0,0 +1,163 @@
+/*
+ * This file is part of Hakbot Origin Controller.
+ *
+ * Hakbot Origin Controller is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Hakbot Origin Controller is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Hakbot Origin Controller. If not, see http://www.gnu.org/licenses/.
+ */
+
+$appspider = function() {
+
+    var status = {};
+    var events = [];
+    var modules = [];
+
+        /**
+     * Populates the DOM with the results of the status object.
+     */
+    function redrawConsole() {
+        // Update progress bar
+        var percentComplete = $('#percentComplete');
+        percentComplete.html(status.scanProgress + "%");
+        percentComplete.css('width', status.scanProgress + "%");
+        if (status.scanProgress = 100 && !status.running) {
+            percentComplete.removeClass("progress-bar-striped");
+        }
+
+        // Update crawling status progress bar
+        var crawlingProgress = $('#crawlingProgress');
+        crawlingProgress.html(status.linksCrawled + "/" + (status.linksCrawled + status.linksInQueue));
+        crawlingProgress.css('width',((status.linksCrawled / status.linksCrawled + status.linksInQueue) * 100 )+ "%");
+        if (status.scanProgress = 100 && !status.running) {
+            crawlingProgress.removeClass("progress-bar-striped");
+        }
+
+        // Update attack status progress bar
+        var attackProgress = $('#attackProgress');
+        attackProgress.html(status.attacked + "/" + (status.attacked + status.attacksInQueue));
+        attackProgress.css('width',((status.attacked / status.attacked + status.attacksInQueue) * 100 )+ "%");
+        if (status.scanProgress = 100 && !status.running) {
+            attackProgress.removeClass("progress-bar-striped");
+        }
+
+        // Update time information
+        $('#startTime').html(status.startTime);
+        $('#endTime').html(status.endTime);
+        $('#elapsedTime').html(status.elapsedTime);
+        $('#timeRemaining').html(status.timeRemaining);
+        $('#consoleTimeTable').bootstrapTable('resetView');
+        
+        // Update network information
+        $('#requests').html(status.requests);
+        $('#failedRequests').html(status.failedRequests);
+        $('#requestDelay').html(status.dripDelay);
+        $('#speed').html(status.networkSpeed);
+        $('#responseTime').html(status.averageResponseTime);
+
+        // Update event information
+        $('#consoleEventTable').bootstrapTable({data: $appspider.events});
+
+        // Update attack module information
+        $('#consoleAttacksTable').bootstrapTable({data: $appspider.modules});
+    }
+
+    /**
+     * Returns the provider-specific job console data
+     */
+    function getJobConsoleData(uuid) {
+        $appspider.uuid = uuid;
+        $.ajax({
+            url: contextPath() + URL_CONSOLE_JOB + "/" + uuid,
+            contentType: CONTENT_TYPE_JSON,
+            dataType: DATA_TYPE,
+            type: METHOD_GET,
+            success: function (data) {
+                parseConsoleData(data);
+                redrawConsole();
+            }
+        });
+    }
+
+    /**
+     * Parses and normalizes the JSON response from the controller creating
+     * a 'status' object with all available properties.
+     */
+    function parseConsoleData(data) {
+        // data is the JSON response from the console
+        status.scanProgress = data.status2.m_iScanProgress;
+        status.timeRemaining = data.status2.m_szTimeRemaining;
+        status.elapsedTime = data.status2.m_szTimeElapsed;
+        status.token = data.Token;
+        status.running = data.Running;
+        status.crashed = data.Crashed;
+        status.scanned = data.Scanned;
+        status.loggedIn = data.LoggedIn;
+        status.linksInQueue = data.LinksInQueue;
+        status.linksCrawled = data.LinksCrawled;
+        status.attacksInQueue = data.AttacksInQueue;
+        status.attacked = data.Attacked;
+        status.vulnerable = data.Vulnerable;
+        status.requests = data.Requests;
+        status.failedRequests = data.FailedRequests;
+        status.networkSpeed = data.NetworkSpeed;
+        status.dripDelay = data.DripDelay;
+        status.averageResponseTime = data.AvgRespTime;
+        status.startTime = timeConverter(data.StartTime);
+        status.endTime = timeConverter(data.EndTime);
+        status.elapsedTime = data.Elapsed;
+
+        for(var i = 0; i < data.EventList.EVENT.length; i++) {
+            var objE = data.EventList.EVENT[i];
+            var event = {};
+            event.timestamp = objE.Time;
+            event.dateTime = timeConverter(objE.Time);
+            event.event = objE.Event;
+            event.details = objE.Details;
+            event.error = objE.Error;
+            event.errorCode = objE.ErrorCode;
+            events.push(event);
+        }
+
+        for(var j = 0; j < data.ModuleStatusList.MODULESTATUS.length; j++) {
+            var objM = data.ModuleStatusList.MODULESTATUS[j];
+            var module = {};
+            module.name = objM.ModuleName;
+            module.passiveAnalysis = objM.PassiveAnalysis;
+            module.attempted = objM.Attempted;
+            module.vulnerable = objM.Vulnerable;
+            modules.push(module);
+        }
+        
+        // Sort events by timestamp
+        events.sort(function(a, b) {
+            return b.timestamp - a.timestamp; // descending order
+        });
+
+        // Sort attack modules by name
+        modules.sort(function(a, b) {
+            if(a.name < b.name) return -1;
+            if(a.name > b.name) return 1;
+            return 0;
+        });
+    }
+
+    /**
+     * Makes the following functions 'public'
+     */
+    return{
+        status:status,
+        events:events,
+        modules:modules,
+        getJobConsoleData:getJobConsoleData
+    }
+
+}();
--- src/main/webapp/WEB-INF/plugins/io.hakbot.providers.appspider.AppSpiderProvider/index.jsp
+++ src/main/webapp/WEB-INF/plugins/io.hakbot.providers.appspider.AppSpiderProvider/index.jsp
@@ -11,11 +11,121 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-12 col-md-12 main" id="main">
-            <h3>AppSpider Pro console coming soon</h3>
-            Job UUID: ${param.uuid}
+            <h3><c:out value="${requestScope.job.name}"/><span style="float:right">AppSpider Pro</span></h3>
+
+            <table id="consoleTimeTable" class="table" data-toggle="table" data-click-to-select="false" data-height="100%">
+                <thead>
+                <tr>
+                    <th data-align="center">Start Time</th>
+                    <th data-align="center">Elapsed Time</th>
+                    <th data-align="center">Time Remaining</th>
+                    <th data-align="center">Completed On</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td id="startTime"></td>
+                    <td id="elapsedTime"></td>
+                    <td id="timeRemaining"></td>
+                    <td id="endTime"></td>
+                </tr>
+                </tbody>
+            </table>
+
         </div>
+
+        <div class="col-sm-9 col-md-9 main">
+            <h4>Overall Progress</h4>
+            <div class="progress">
+                <div id="percentComplete" class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width:2em; width:0%">
+                </div>
+            </div>
+
+            <h4>Crawling Progress</h4>
+            <div class="progress">
+                <div id="crawlingProgress" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width:2em; width:0%">
+                </div>
+            </div>
+
+            <h4>Attack Progress</h4>
+            <div class="progress">
+                <div id="attackProgress" class="progress-bar progress-bar-danger progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="min-width:2em; width:0%">
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3 col-md-3 main">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Network Monitor</h3>
+                </div>
+                <table class="table">
+                    <tr>
+                        <td>Requests:</td>
+                        <td id="requests"></td>
+                    </tr>
+                    <tr>
+                        <td>Failed Requests:</td>
+                        <td id="failedRequests"></td>
+                    </tr>
+                    <tr>
+                        <td>Request Delay (ms):</td>
+                        <td id="requestDelay"></td>
+                    </tr>
+                    <tr>
+                        <td>Speed (KB/sec):</td>
+                        <td id="speed"></td>
+                    </tr>
+                    <tr>
+                        <td>Response Time (ms):</td>
+                        <td id="responseTime"></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-sm-12 col-md-12 main">
+            <div class="panel with-nav-tabs panel-default">
+                <div class="panel-heading">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#eventsTab" data-toggle="tab">Events</a></li>
+                        <li><a href="#attacksTab" data-toggle="tab">Attacks</a></li>
+                    </ul>
+                </div>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="eventsTab">
+                            <table id="consoleEventTable" class="table">
+                                <thead>
+                                <tr>
+                                    <th data-align="left" data-field="dateTime">Date/Time</th>
+                                    <th data-align="left" data-field="event">Description</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                        <div class="tab-pane" id="attacksTab">
+                            <table id="consoleAttacksTable" class="table">
+                                <thead>
+                                <tr>
+                                    <th data-align="left" data-field="name">Module</th>
+                                    <th data-align="left" data-field="attempted">Attempted</th>
+                                    <th data-align="left" data-field="vulnerable">Vulnerable</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
 <jsp:include page="/WEB-INF/fragments/footer.jsp"/>
+<script type="text/javascript" src="<c:url value="/console/static/io.hakbot.providers.appspider.AppSpiderProvider/functions.js"/>"></script>
+<script type="text/javascript">
+    $appspider.getJobConsoleData('<c:out value="${requestScope.job.uuid}"/>');
+</script>
 </body>
 </html>
