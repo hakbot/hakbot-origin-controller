93550160264110134f5a3c26a4cb6932a8b3dab0
stevespringett <steve@springett.us>
2016-10-12 19:55:28 -0500

    Removing unused XML attributes

--- src/main/java/io/hakbot/controller/workers/State.java
+++ src/main/java/io/hakbot/controller/workers/State.java
@@ -16,10 +16,6 @@
  */
 package io.hakbot.controller.workers;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlType;
-
 /**
  * This Enum class represents all of the possible states a Job can have.
  * <li>In Queue - The job is in the queue and is waiting for available job slots to become available.</li>
@@ -29,17 +25,15 @@ import javax.xml.bind.annotation.XmlType;
  * <li>Paused - The thread executing a job was requested to pause execution. Paused jobs still take up available job slots because the threads executing them are still active.</li>
  * <li>Unavailable - A job in the queue was scheduled to be executed (in progress) but the provider was not available to process the job.</li>
  */
-@XmlType
-@XmlEnum(String.class)
 public enum	State {
 
-    @XmlEnumValue("Created") CREATED("Created"),
-    @XmlEnumValue("In Queue") IN_QUEUE("In Queue"),
-    @XmlEnumValue("In Progress") IN_PROGRESS("In Progress"),
-    @XmlEnumValue("Completed") COMPLETED("Completed"),
-    @XmlEnumValue("Published") PUBLISHED("Published"),
-    @XmlEnumValue("Canceled") CANCELED("Canceled"),
-    @XmlEnumValue("Unavailable") UNAVAILABLE("Unavailable");
+    CREATED("Created"),
+    IN_QUEUE("In Queue"),
+    IN_PROGRESS("In Progress"),
+    COMPLETED("Completed"),
+    PUBLISHED("Published"),
+    CANCELED("Canceled"),
+    UNAVAILABLE("Unavailable");
 
     String stateName;
     State(String state) {
