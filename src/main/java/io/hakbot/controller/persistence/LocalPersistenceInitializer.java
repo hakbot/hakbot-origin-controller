aa621f8b4f8845ce8f0cfdd988dbcfbfecc62070
stevespringett <steve@springett.us>
2016-12-10 14:12:14 -0600

    Changed dbshutdown procedure

--- src/main/java/io/hakbot/controller/persistence/LocalPersistenceInitializer.java
+++ src/main/java/io/hakbot/controller/persistence/LocalPersistenceInitializer.java
@@ -67,8 +67,10 @@ public class LocalPersistenceInitializer implements ServletContextListener {
 
     private void stopDbServer() {
         logger.info("Shutting down database service");
-        if (dbServer != null)
+        if (dbServer != null) {
+            dbServer.stop();
             dbServer.shutdown();
+        }
     }
 
 }
