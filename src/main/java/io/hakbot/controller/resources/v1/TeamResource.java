fb480325d71959821f65beb362caa0db5e54e740
stevespringett <steve@springett.us>
2017-01-14 19:10:49 -0600

    Added apikey admin functionality.

--- src/main/java/io/hakbot/controller/resources/v1/TeamResource.java
+++ src/main/java/io/hakbot/controller/resources/v1/TeamResource.java
@@ -16,12 +16,18 @@
  */
 package io.hakbot.controller.resources.v1;
 
+import io.hakbot.controller.model.ApiKey;
 import io.hakbot.controller.model.Team;
 import io.hakbot.controller.persistence.QueryManager;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -47,6 +53,82 @@ public class TeamResource extends BaseResource {
         List<Team> teams = qm.getTeams();
         qm.close();
         return Response.ok(teams).build();
+    }
+
+    @PUT
+    @Path("/{uuid}/key")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Generates an API key and returns its value",
+            notes = "Requires hakmaster permission.",
+            response = ApiKey.class
+    )
+    public Response generateApiKey(
+            @ApiParam(value = "The UUID of the team to generate a key for", required = true)
+            @PathParam("uuid") String uuid) {
+        if (!isHakmaster()) {
+            Response.status(Response.Status.UNAUTHORIZED);
+        }
+        QueryManager qm = new QueryManager();
+        Team team = qm.getObjectByUuid(Team.class, uuid);
+        if (team != null) {
+            ApiKey apiKey = qm.createApiKey(team);
+            qm.close();
+            return Response.ok(apiKey).build();
+        } else {
+            qm.close();
+            return Response.notModified().build();
+        }
+    }
+
+    @POST
+    @Path("/key/{apikey}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Regenerates an API key by removing the specified key, generating a new one and returning its value",
+            notes = "Requires hakmaster permission.",
+            response = ApiKey.class
+    )
+    public Response regenerateApiKey(
+            @ApiParam(value = "The API key to regenerate", required = true)
+            @PathParam("apikey") String apikey) {
+        if (!isHakmaster()) {
+            Response.status(Response.Status.UNAUTHORIZED);
+        }
+        QueryManager qm = new QueryManager();
+        ApiKey apiKey = qm.getApiKey(apikey);
+        if (apiKey != null) {
+            apiKey = qm.regenerateApiKey(apiKey);
+            qm.close();
+            return Response.ok(apiKey).build();
+        } else {
+            qm.close();
+            return Response.notModified().build();
+        }
+    }
+
+    @DELETE
+    @Path("/key/{apikey}")
+    @ApiOperation(
+            value = "Deletes the specified API key",
+            notes = "Requires hakmaster permission."
+    )
+    public Response deleteApiKey(
+            @ApiParam(value = "The API key to delete", required = true)
+            @PathParam("apikey") String apikey) {
+        if (!isHakmaster()) {
+            Response.status(Response.Status.UNAUTHORIZED);
+        }
+        QueryManager qm = new QueryManager();
+        ApiKey apiKey = qm.getApiKey(apikey);
+        if (apiKey != null) {
+            qm.delete(apiKey);
+            qm.close();
+            return Response.ok().build();
+        } else {
+            qm.close();
+            return Response.notModified("The API key could not be found").build();
+        }
     }
 
 }
--- src/main/webapp/admin/functions.js
+++ src/main/webapp/admin/functions.js
@@ -20,8 +20,16 @@
  */
 function formatTeamTable(res) {
     for (var i=0; i<res.length; i++) {
-        res[i].apiKeysNum = res[i].apiKeys.length;
-        res[i].membersNum = res[i].ldapUsers.length;
+        if (res[i].apiKeys === undefined) {
+            res[i].apiKeysNum = 0;
+        } else {
+            res[i].apiKeysNum = res[i].apiKeys.length;
+        }
+        if (res[i].ldapUsers === undefined) {
+            res[i].membersNum = 0;
+        } else {
+            res[i].membersNum = res[i].ldapUsers.length;
+        }
 
         if (res[i].hakmaster == true) {
             res[i].hakmasterIcon = "&#10004;";
@@ -31,7 +39,6 @@ function formatTeamTable(res) {
     }
     return res;
 }
-
 
 /**
  * Called by bootstrap table to format the data in the table.
@@ -47,9 +54,19 @@ $teamTable.on("click-row.bs.table", function(e, row, $tr) {
     //console.log("Clicked on: " + $(e.target).attr('class'), [e, row, $tr]);
     if ($tr.next().is('tr.detail-view')) {
         $teamTable.bootstrapTable('collapseRow', $tr.data('index'));
+        $teamTable.expanded = false;
     } else {
         $teamTable.bootstrapTable('collapseAllRows');
         $teamTable.bootstrapTable('expandRow', $tr.data('index'));
+        $teamTable.expanded = true;
+        $teamTable.expandedRow = $tr.data('index');
+    }
+});
+
+$teamTable.on("load-success.bs.table", function(data) {
+    if ($teamTable.expanded == true) {
+        console.log("expanding: " + $teamTable.expandedRow);
+        $teamTable.bootstrapTable('expandRow', $teamTable.expandedRow);
     }
 });
 
@@ -65,28 +82,41 @@ function teamDetailFormatter(index, row) {
 
 
     var apiKeysHtml = '';
-    for (i = 0; i < row.apiKeys.length; i++) {
-        apiKeysHtml += `
-            <li class="list-group-item">
-                <a href="#" onclick="alert('Not yet implemented')" data-toggle="tooltip" title="Delete API Key">
+    if (!(row.apiKeys === undefined)) {
+        for (i = 0; i < row.apiKeys.length; i++) {
+            apiKeysHtml += `
+            <li class="list-group-item" id="container-apikey-${row.apiKeys[i].key}">
+                <a href="#" id="delete-${row.apiKeys[i].key}" onclick="deleteApiKey('${row.apiKeys[i].key}')" data-toggle="tooltip" title="Delete API Key">
                     <span class="glyphicon glyphicon-trash glyphicon-input-form pull-right"></span>
                 </a>
-                <a href="#" onclick="alert('Not yet implemented')" data-toggle="tooltip" title="Regenerate New API Key">
+                <a href="#" id="regen-${row.apiKeys[i].key}" onclick="regenerateApiKey('${row.apiKeys[i].key}')" data-toggle="tooltip" title="Regenerate New API Key">
                     <span class="glyphicon glyphicon-refresh glyphicon-input-form pull-right spacer-horizontal-10"></span>
                 </a>
-                ${row.apiKeys[i].key}
+                <span id="apikey-${row.apiKeys[i].key}">${row.apiKeys[i].key}</span>
             </li>`;
+        }
     }
+    apiKeysHtml += `
+            <li class="list-group-item" id="container-no-apikey">
+                <a href="#" id="add-apikey" onclick="addApiKey('${row.uuid}')" data-toggle="tooltip" title="Add API Key">
+                    <span class="glyphicon glyphicon-plus-sign glyphicon-input-form pull-right"></span>
+                </a>
+                <span>&nbsp;</span>
+            </li>`;
+
+
 
     var membersHtml = '';
-    for (i = 0; i < row.ldapUsers.length; i++) {
-        membersHtml += `
+    if (!(row.ldapUsers === undefined)) {
+        for (i = 0; i < row.ldapUsers.length; i++) {
+            membersHtml += `
             <li class="list-group-item">
-                <a href="#" onclick="alert('Not yet implemented')" data-toggle="tooltip" title="Remove User From Team">
+                <a href="#" onclick="removeTeamMembership('${row.uuid}', '${row.ldapUsers[i].username}')" data-toggle="tooltip" title="Remove User From Team">
                     <span class="glyphicon glyphicon-trash glyphicon-input-form pull-right"></span>
                 </a>
                 ${row.ldapUsers[i].username}
             </li>`;
+        }
     }
 
     var template = `
@@ -104,7 +134,7 @@ function teamDetailFormatter(index, row) {
         </div> 
         <div class="form-group">
             <label for="inputApiKeys">Hakmaster</label>
-            <input type="checkbox" class="checkbox-inline" id="inputTeamHakmaster" placeholder="Hakmaster" ${hakmasterChecked}>
+            <input type="checkbox" class="checkbox-inline" id="inputTeamHakmaster" placeholder="Hakmaster" ${hakmasterChecked} onchange="setHakmaster('${row.uuid}', this.checked)">
         </div> 
     </div>
     <div class="col-sm-6 col-md-6">
@@ -127,152 +157,62 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-$admin = function() {
-
-    var status = {};
-    var events = [];
-    var modules = [];
-
-    /**
-     * Populates the DOM with the results of the status object.
-     */
-    function redrawConsole() {
-        // Update progress bar
-        var percentComplete = $('#percentComplete');
-        percentComplete.html(status.scanProgress + "%");
-        percentComplete.css('width', status.scanProgress + "%");
-        if (status.scanProgress = 100 && !status.running) {
-            percentComplete.removeClass("progress-bar-striped");
+function deleteApiKey(apikey) {
+    $.ajax({
+        url: contextPath() + URL_TEAM + "/key/" + apikey,
+        contentType: CONTENT_TYPE_JSON,
+        type: METHOD_DELETE,
+        success: function () {
+            $('#container-apikey-' + apikey).remove();
+            $teamTable.bootstrapTable('refresh', {silent: true});
+        },
+        error: function(xhr, ajaxOptions, thrownError){
+            console.log("failed");
         }
+    });
+}
 
-        // Update crawling status progress bar
-        var crawlingProgress = $('#crawlingProgress');
-        crawlingProgress.html(status.linksCrawled + "/" + (status.linksCrawled + status.linksInQueue));
-        crawlingProgress.css('width',((status.linksCrawled / (status.linksCrawled + status.linksInQueue)) * 100 )+ "%");
-        if (status.scanProgress = 100 && !status.running) {
-            crawlingProgress.removeClass("progress-bar-striped");
+function regenerateApiKey(apikey) {
+    $.ajax({
+        url: contextPath() + URL_TEAM + "/key/" + apikey,
+        contentType: CONTENT_TYPE_JSON,
+        dataType: DATA_TYPE,
+        type: METHOD_POST,
+        success: function (data) {
+            $('#apikey-' + apikey).html(data.key);
+            $('#apikey-' + apikey).attr("id","apikey-" + data.key);
+            $('#regen-' + apikey).attr("id","regen-" + data.key);
+            $('#regen-' + data.key).attr("onclick","regenerateApiKey('" + data.key + "')");
+            $('#delete-' + apikey).attr("id","delete-" + data.key);
+            $('#delete-' + data.key).attr("onclick","deleteApiKey('" + data.key + "')");
+            $teamTable.bootstrapTable('refresh', {silent: true});
+        },
+        error: function(xhr, ajaxOptions, thrownError){
+            console.log("failed");
         }
+    });
+}
 
-        // Update attack status progress bar
-        var attackProgress = $('#attackProgress');
-        attackProgress.html(status.attacked + "/" + (status.attacked + status.attacksInQueue));
-        attackProgress.css('width',((status.attacked / (status.attacked + status.attacksInQueue)) * 100 )+ "%");
-        if (status.scanProgress = 100 && !status.running) {
-            attackProgress.removeClass("progress-bar-striped");
+function addApiKey(uuid) {
+    console.log("adding api key for team: " + uuid);
+    $.ajax({
+        url: contextPath() + URL_TEAM + "/" + uuid + "/key",
+        contentType: CONTENT_TYPE_JSON,
+        dataType: DATA_TYPE,
+        type: METHOD_PUT,
+        success: function (data) {
+            $teamTable.bootstrapTable('refresh', {silent: true});
+        },
+        error: function(xhr, ajaxOptions, thrownError){
+            console.log("failed");
         }
+    });
+}
 
-        // Update time information
-        $('#startTime').html(status.startTime);
-        $('#endTime').html(status.endTime);
-        $('#elapsedTime').html(status.elapsedTime);
-        $('#timeRemaining').html(status.timeRemaining);
-        $('#consoleTimeTable').bootstrapTable('resetView');
+function setHakmaster(uuid, isHakmaster) {
+    alert("Setting hakmaster status of team: " + uuid + " to: " + isHakmaster);
+}
 
-        // Update network information
-        $('#requests').html(status.requests);
-        $('#failedRequests').html(status.failedRequests);
-        $('#requestDelay').html(status.dripDelay);
-        var kbpersec = Math.round((status.networkSpeed/1000) * 100) / 100;
-        $('#speed').html(kbpersec);
-        $('#responseTime').html(status.averageResponseTime);
-
-        // Update event information
-        $('#consoleEventTable').bootstrapTable({data: $appspider.events});
-
-        // Update attack module information
-        $('#consoleAttacksTable').bootstrapTable({data: $appspider.modules});
-    }
-
-    /**
-     * Returns the provider-specific job console data
-     */
-    function getJobConsoleData(uuid) {
-        $appspider.uuid = uuid;
-        $.ajax({
-            url: contextPath() + URL_CONSOLE_JOB + "/" + uuid,
-            contentType: CONTENT_TYPE_JSON,
-            dataType: DATA_TYPE,
-            type: METHOD_GET,
-            success: function (data) {
-                parseConsoleData(data);
-                redrawConsole();
-            }
-        });
-    }
-
-    /**
-     * Parses and normalizes the JSON response from the controller creating
-     * a 'status' object with all available properties.
-     */
-    function parseConsoleData(data) {
-        // data is the JSON response from the console
-        status.scanProgress = data.status2.m_iScanProgress;
-        status.timeRemaining = data.status2.m_szTimeRemaining;
-        status.elapsedTime = data.status2.m_szTimeElapsed;
-        status.token = data.Token;
-        status.running = data.Running;
-        status.crashed = data.Crashed;
-        status.scanned = data.Scanned;
-        status.loggedIn = data.LoggedIn;
-        status.linksInQueue = data.LinksInQueue;
-        status.linksCrawled = data.LinksCrawled;
-        status.attacksInQueue = data.AttacksInQueue;
-        status.attacked = data.Attacked;
-        status.vulnerable = data.Vulnerable;
-        status.requests = data.Requests;
-        status.failedRequests = data.FailedRequests;
-        status.networkSpeed = data.NetworkSpeed;
-        status.dripDelay = data.DripDelay;
-        status.averageResponseTime = data.AvgRespTime;
-        status.startTime = timeConverter(data.StartTime);
-        status.endTime = timeConverter(data.EndTime);
-        status.elapsedTime = data.Elapsed;
-
-        events.length = 0; // Empties array by setting length to 0. Keeps references to object in-tact.
-        for(var i = 0; i < data.EventList.EVENT.length; i++) {
-            var objE = data.EventList.EVENT[i];
-            var event = {};
-            event.timestamp = objE.Time;
-            event.dateTime = timeConverter(objE.Time);
-            event.event = objE.Event;
-            event.details = objE.Details;
-            event.error = objE.Error;
-            event.errorCode = objE.ErrorCode;
-            events.push(event);
-        }
-
-        modules.length = 0; // Empties array by setting length to 0. Keeps references to object in-tact.
-        for(var j = 0; j < data.ModuleStatusList.MODULESTATUS.length; j++) {
-            var objM = data.ModuleStatusList.MODULESTATUS[j];
-            var module = {};
-            module.name = objM.ModuleName;
-            module.passiveAnalysis = objM.PassiveAnalysis;
-            module.attempted = objM.Attempted;
-            module.vulnerable = objM.Vulnerable;
-            modules.push(module);
-        }
-
-        // Sort events by timestamp
-        events.sort(function(a, b) {
-            return b.timestamp - a.timestamp; // descending order
-        });
-
-        // Sort attack modules by name
-        modules.sort(function(a, b) {
-            if(a.name < b.name) return -1;
-            if(a.name > b.name) return 1;
-            return 0;
-        });
-    }
-
-    /**
-     * Makes the following functions 'public'
-     */
-    return{
-        status:status,
-        events:events,
-        modules:modules,
-        getJobConsoleData:getJobConsoleData
-    }
-
-}();
+function removeTeamMembership(uuid, username) {
+    alert("Removing membership of " + username + " from group: " + uuid);
+}
